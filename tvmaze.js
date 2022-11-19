9727e208ebb85ef00ec2b061224aa1006141c633
alexm <alex-m@outlook.com>
2022-11-18 17:24:42 -0800

    Commented out the hard-coded array of show results; replaced hard-coded image source in populateShows() with image retrieved from shows argument; adjusted spacing

--- tvmaze.js
+++ tvmaze.js
@@ -18,24 +18,30 @@ const $searchForm = $("#searchForm");
 async function getShowsByTerm(searchTerm) {
     // ADD: make request to TVMaze search shows API.
 
-    return [
-    {
-        id: 1767,
-        name: "The Bletchley Circle",
-        summary:
-            `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-            women with extraordinary skills that helped to end World War II.</p>
-            <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-            normal lives, modestly setting aside the part they played in
-            producing crucial intelligence, which helped the Allies to victory
-            and shortened the war. When Susan discovers a hidden code behind an
-            unsolved murder she is met by skepticism from the police. She
-            quickly realises she can only begin to crack the murders and bring
-            the culprit to justice with her former friends.</p>`,
-        image:
-            "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-    ];
+    const showsArr = [];
+    
+
+
+    return showsArr;
+
+    // return [
+    // {
+    //     id: 1767,
+    //     name: "The Bletchley Circle",
+    //     summary:
+    //         `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
+    //         women with extraordinary skills that helped to end World War II.</p>
+    //         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+    //         normal lives, modestly setting aside the part they played in
+    //         producing crucial intelligence, which helped the Allies to victory
+    //         and shortened the war. When Susan discovers a hidden code behind an
+    //         unsolved murder she is met by skepticism from the police. She
+    //         quickly realises she can only begin to crack the murders and bring
+    //         the culprit to justice with her former friends.</p>`,
+    //     image:
+    //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+    // }
+    // ];
 }
 
 
@@ -48,7 +54,7 @@ function populateShows(shows) {
             `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
                 <div class="media">
                 <img
-                    src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+                    src=${show.image}
                     alt="Bletchley Circle San Francisco"
                     class="w-25 me-3">
                 <div class="media-body">
@@ -70,11 +76,10 @@ function populateShows(shows) {
 /** Handle search form submission: get shows from API and display them.
  *    Hide the episodes area (that only gets shown if they ask for episodes).
  */
-
 async function searchForShowAndDisplay() {
     const term = $("#searchForm-term").val();
     const shows = await getShowsByTerm(term);
-    
+
     $episodesArea.hide();
     populateShows(shows);
 }
@@ -88,8 +93,8 @@ $searchForm.on("submit", async function (evt) {
 /** Given a show ID, get the show from API and return (promise) array of episodes:
  *  { id, name, season, number }
  */
-
 async function getEpisodesOfShow(id) {}
+
 
 /** Write a clear docstring for this function... */
 function populateEpisodes(episodes) {}
